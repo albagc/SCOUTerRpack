eae21195c37110e2c93a7f1e43678ebcd3873746
Alba GC <albagoncebrian@gmail.com>
2020-06-22 16:04:02 +0200

    Include demo data

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Demo dataset with 50 observations and 5 normally distributed variables with 
+#' two Principal Components explaining the 80% of the total variance. 
+#'
+#' It is a small data set to use as a demo for the SCOUTer package. 
+#'
+#' @format A data frame with 50 rows and 5 normally distributed variables.
+"X"
